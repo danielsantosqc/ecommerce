27f6bb9bd93ac96d20900cdc7db626892582a73c
daniel <danielisan@web.com>
2019-11-03 07:20:44 -0300

    autoload php-clases DB

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-    echo "Iniciando o Projeto";
+    $sql = new DB\Sql();
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 });
 
 $app->run();
